3c9db6526709419771d3ea0ffede117b58bfcb92
sedanova <sedanovaalina@gmail.com>
2024-10-10 12:08:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return """<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>New Website</title>
+                </head>
+                <body>
+                    <style>
+                        div{
+                            width: 100%;
+                            height: 100vh;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                        }
+                    </style>
+                    <div>
+                        <span>
+                            Warrior x
+                        </span>
+                    </div>
+                </body>
+                </html>"""
 
 
 if __name__ == '__main__':
